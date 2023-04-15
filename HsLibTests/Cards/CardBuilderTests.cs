076a61ede08884cd040626ed13d4f3525f7d93f2
Niakr1s <pavel2188@gmail.com>
2023-04-15 15:43:01 +0300

    added tests for CardBuilder

---
+++ HsLibTests/Cards/CardBuilderTests.cs
@@ -0,0 +1,16 @@
+﻿namespace HsLib.Cards.Tests
+{
+    [TestClass()]
+    public class CardBuilderTests
+    {
+        [TestMethod()]
+        public void FromIdTest()
+        {
+            foreach (CardId id in Enum.GetValues(typeof(CardId)))
+            {
+                // checking it doesn't throw exception
+                CardBuilder.FromId(id);
+            }
+        }
+    }
+}
