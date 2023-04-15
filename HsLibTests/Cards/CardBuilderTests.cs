4e3fbfc9299caa7ef614a5da908777c42eb7d705
Niakr1s <pavel2188@gmail.com>
2023-04-15 15:51:13 +0300

    added output to CardBuilderTests

--- HsLibTests/Cards/CardBuilderTests.cs
+++ HsLibTests/Cards/CardBuilderTests.cs
@@ -3,13 +3,17 @@
     [TestClass()]
     public class CardBuilderTests
     {
+        public TestContext? TestContext { get; set; }
+
         [TestMethod()]
         public void FromIdTest()
         {
             foreach (CardId id in Enum.GetValues(typeof(CardId)))
             {
                 // checking it doesn't throw exception
+                TestContext?.Write($"Test: {id}");
                 CardBuilder.FromId(id);
+                TestContext?.WriteLine(" => success");
             }
         }
     }
