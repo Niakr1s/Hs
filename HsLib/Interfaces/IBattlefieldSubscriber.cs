6efb7fb6b9ae6bf774c827ef8598f60b4ca92435
Niakr1s <pavel2188@gmail.com>
2023-05-15 20:56:42 +0300

    extracted ICard event-related interfaces to IBattlefieldSUbscriber

---
+++ HsLib/Interfaces/IBattlefieldSubscriber.cs
@@ -0,0 +1,12 @@
+﻿using HsLib.Systems;
+
+namespace HsLib.Interfaces
+{
+    public interface IBattlefieldSubscriber
+    {
+        void AfterContainerInsert(Battlefield bf);
+        void AfterContainerRemove(Battlefield bf);
+        void OnTurnEnd(Battlefield bf);
+        void OnTurnStart(Battlefield bf);
+    }
+}
--- HsLib/Interfaces/ICard.cs
+++ HsLib/Interfaces/ICard.cs
@@ -1,18 +1,12 @@
-﻿using HsLib.Systems;
-using HsLib.Types;
+﻿using HsLib.Types;
 using HsLib.Types.Stats;
 
 namespace HsLib.Interfaces
 {
-    public interface ICard
+    public interface ICard : IBattlefieldSubscriber
     {
         PlaceInContainer? Place { get; set; }
 
         Mp Mp { get; }
-
-        void AfterContainerInsert(Battlefield bf);
-        void AfterContainerRemove(Battlefield bf);
-        void OnTurnEnd(Battlefield bf);
-        void OnTurnStart(Battlefield bf);
     }
 }
