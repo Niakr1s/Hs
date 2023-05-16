b2d04327794c9c22ca444fd589ac608ab715c1a4
Niakr1s <pavel2188@gmail.com>
2023-05-16 20:28:38 +0300

    added doc

--- HsLib/Interfaces/IActiveEffectWithValidation.cs
+++ HsLib/Interfaces/IActiveEffectWithValidation.cs
@@ -5,6 +5,13 @@ namespace HsLib.Interfaces
 {
     public interface IActiveEffectWithValidation : IActiveEffect
     {
+        /// <summary>
+        /// Validates of validity or effect target while playing from hand.
+        /// </summary>
+        /// <param name="bf"></param>
+        /// <param name="pid"></param>
+        /// <param name="effectTarget"></param>
+        /// <exception cref="ValidationException"></exception>
         void ValidateEffectTarget(Battlefield bf, Pid pid, ICard? effectTarget);
     }
 }
