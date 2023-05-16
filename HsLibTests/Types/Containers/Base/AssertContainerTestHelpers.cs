2f2e696c02ac2840499b2131320bb7590a08d9db
Niakr1s <pavel2188@gmail.com>
2023-05-16 13:16:52 +0300

    cleaned contaner tests + extracted helpers to single file

---
+++ HsLibTests/Types/Containers/Base/AssertContainerTestHelpers.cs
@@ -0,0 +1,20 @@
+﻿using HsLib.Interfaces;
+using HsLib.Types.Containers.Base;
+
+namespace HsLibTests.Types.Containers.Base
+{
+    internal static class AssertContainerTestHelpers
+    {
+        internal static void AssertCardsHaveValidPlaces(IContainer container)
+        {
+            Assert.IsNotNull(container.Place);
+            for (int i = 0; i < container.Count; i++)
+            {
+                ICard card = (ICard)container[i]!;
+                Assert.IsNotNull(card.Place);
+                Assert.AreEqual(container.Place, card.Place);
+                Assert.AreEqual(i, card.Place.Index);
+            }
+        }
+    }
+}
