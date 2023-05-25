df55ff907cfe1f2e54d9b31d61512a003a27f107
Niakr1s <pavel2188@gmail.com>
2023-05-25 08:08:52 +0300

    added HealEffect

---
+++ HsLib/Types/Effects/HealEffect.cs
@@ -0,0 +1,22 @@
+﻿using HsLib.Systems;
+using HsLib.Types.Cards;
+
+namespace HsLib.Types.Effects
+{
+    public class HealEffect : IEffect
+    {
+        public int Heal { get; set; }
+
+        public Action UseEffect(Battlefield bf, ICard owner, ICard? target)
+        {
+            if (target is IDamageable d)
+            {
+                return () => d.Hp.Increase(Heal);
+            }
+            else
+            {
+                throw new ValidationException("target can't be healed");
+            }
+        }
+    }
+}
